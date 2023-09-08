517ee6a2329d66df491df4d5a43a7a01a91ea281
Sergey Opria <seeruyy@gmail.com>
2023-09-07 22:45:13 -0400

    feat(trace): added trace protobuf parse and validation
    Added OpenTelemetry trace protobuf decoding and validation.
    
    Ref: LOG-18072

---
+++ src/validation/trace.rs
@@ -0,0 +1,98 @@
+use url::Url;
+
+use crate::opentelemetry::trace::{
+    ResourceSpans, ScopeSpans, Span, SpanEvent, SpanLink, Status, TracesData,
+};
+
+use crate::validation::common::*;
+
+pub trait TraceValidate {
+    fn validate(&self) -> Result<(), crate::Error>;
+}
+
+impl TraceValidate for TracesData<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        for resource_spans in &self.resource_spans {
+            resource_spans.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for ResourceSpans<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        match &self.resource {
+            Some(resource) => resource.validate(),
+            None => Ok(()),
+        }?;
+
+        for scope_spans in &self.scope_spans {
+            scope_spans.validate()?;
+        }
+
+        if self.schema_url != "" {
+            Url::parse(self.schema_url.as_ref())?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for ScopeSpans<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        for span in &self.spans {
+            span.validate()?;
+        }
+
+        if self.schema_url != "" {
+            Url::parse(self.schema_url.as_ref())?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for Span<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        for attribute in &self.attributes {
+            attribute.validate()?;
+        }
+
+        for event in &self.events {
+            event.validate()?;
+        }
+
+        for link in &self.links {
+            link.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for SpanEvent<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        for attribute in &self.attributes {
+            attribute.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for SpanLink<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        for attribute in &self.attributes {
+            attribute.validate()?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TraceValidate for Status<'_> {
+    fn validate(&self) -> Result<(), crate::Error> {
+        Ok(())
+    }
+}
